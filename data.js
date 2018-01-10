e258d631fdf040a8f40dcfad8cc9ea4b36fb19ad
e <123@qq.com>
2018-01-10 20:53:40 +0800

    haha

---
+++ data.js
@@ -0,0 +1,22 @@
+var classInfo = {
+	info: {
+		name: "web170918",
+		num: "16"
+	},
+	man: [{
+		name: "周一帆",
+		age: 20
+	}, {
+		name: "李娜",
+		age: 30
+	}],
+	woman: [{
+		name: "许志明",
+		age: 39
+	}, {
+		name: "刘浩然",
+		age: 5
+	}]
+}
+
+module.exports = classInfo;
